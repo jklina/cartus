0834f17d0aa4e11110e70106965275fc03fc3720
Josh Klina <joshua.klina@gmail.com>
2020-02-14 16:50:58 -0700

    chore: test sign in redirect

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
-    it "returns http success" do
+    it "redirects to the sign_in_path when not signed in" do
       get :index
       expect(response).to redirect_to sign_in_path
+    end
+
+    it "redirects to the user's path when signed in" do
+      user = create(:user)
+      sign_in_as(user)
+
+      get :index
+
+      expect(response).to redirect_to user_path(user)
     end
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "user#{n}@smartcare.io" }
+    password   { "password" }
+  end
+end
