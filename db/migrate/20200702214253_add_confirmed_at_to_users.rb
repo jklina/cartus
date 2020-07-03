1c41d554c906dfc0df907f7ac84a8123db848323
Josh Klina <joshua.klina@gmail.com>
2020-07-02 21:40:52 -0600

    Backfill email confirmations

--- db/migrate/20200702214253_add_confirmed_at_to_users.rb
+++ db/migrate/20200702214253_add_confirmed_at_to_users.rb
@@ -2,5 +2,6 @@ class AddConfirmedAtToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :email_confirmation_token, :string, null: false, default: ""
     add_column :users, :email_confirmed_at, :datetime
+    User.all.update_all(email_confirmed_at: Date.today)
   end
 end
